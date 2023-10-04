6d685f17165973cb7a7b0ebc39bc813a6355d4c4
nadineprofesseur <16899413+nadineprofesseur@users.noreply.github.com>
2023-10-04 13:41:16 -0400

    Objet test pour ajouter

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import architecture.Controleur;
 import architecture.Fenetre;
 import donnee.BateauDAO;
 import donnee.FlotteDAO;
+import modele.Bateau;
 import modele.Flotte;
 import vue.VueAjouterBateau;
 import vue.VueFlotte;
@@ -13,13 +14,22 @@ public class App {
 	public static void main(String[] parametres) {
 		
 		
-		//BateauDAO bateauDAO = new BateauDAO();
+		BateauDAO bateauDAO = new BateauDAO();
 		//bateauDAO.listerBateauxParFlotte(1);
 		//flotteDAO.detaillerFlotte(1);
+		Bateau fauxBateau = new Bateau();
+		fauxBateau.setNom("faux");
+		fauxBateau.setValeur(4);
+		fauxBateau.setNoeud(25);
+		fauxBateau.setIdFlotte(1);
+		bateauDAO.ajouterBateau(fauxBateau);
+		
 		//Controleur.choisirVuePrincipale(VueAjouterBateau.class);
 		//Controleur.choisirVuePrincipale(VueFlotte.class);
-		Controleur.choisirVuePrincipale(VueMarina.class);
-		Fenetre.launch(Fenetre.class, parametres);	
+		
+		
+		//Controleur.choisirVuePrincipale(VueMarina.class);
+		//Fenetre.launch(Fenetre.class, parametres);	
 	}
 
 }
