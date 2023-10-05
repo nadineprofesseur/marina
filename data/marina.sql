f0dc80db3584570312317b39f43617392862d455
nadineprofesseur <16899413+nadineprofesseur@users.noreply.github.com>
2023-10-05 14:59:01 -0400

    SQL final avec jeux de donnees

--- data/marina.sql
+++ data/marina.sql
@@ -1,3 +1,7 @@
+--
+-- Name: bateau; Type: TABLE; Schema: public; Owner: -
+--
+
 CREATE TABLE public.bateau (
     id integer NOT NULL,
     nom text,
@@ -7,7 +11,6 @@ CREATE TABLE public.bateau (
 
 
 --
--- TOC entry 217 (class 1259 OID 32991)
 -- Name: bateau_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -21,8 +24,6 @@ CREATE SEQUENCE public.bateau_id_seq
 
 
 --
--- TOC entry 3335 (class 0 OID 0)
--- Dependencies: 217
 -- Name: bateau_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -30,7 +31,6 @@ ALTER SEQUENCE public.bateau_id_seq OWNED BY public.bateau.id;
 
 
 --
--- TOC entry 215 (class 1259 OID 24792)
 -- Name: flotte; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -41,7 +41,6 @@ CREATE TABLE public.flotte (
 
 
 --
--- TOC entry 214 (class 1259 OID 24791)
 -- Name: flotte_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -55,8 +54,6 @@ CREATE SEQUENCE public.flotte_id_seq
 
 
 --
--- TOC entry 3336 (class 0 OID 0)
--- Dependencies: 214
 -- Name: flotte_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -64,7 +61,6 @@ ALTER SEQUENCE public.flotte_id_seq OWNED BY public.flotte.id;
 
 
 --
--- TOC entry 3179 (class 2604 OID 32992)
 -- Name: bateau id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -72,7 +68,6 @@ ALTER TABLE ONLY public.bateau ALTER COLUMN id SET DEFAULT nextval('public.batea
 
 
 --
--- TOC entry 3178 (class 2604 OID 24795)
 -- Name: flotte id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -80,8 +75,6 @@ ALTER TABLE ONLY public.flotte ALTER COLUMN id SET DEFAULT nextval('public.flott
 
 
 --
--- TOC entry 3328 (class 0 OID 32988)
--- Dependencies: 216
 -- Data for Name: bateau; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -91,11 +84,14 @@ INSERT INTO public.bateau VALUES (3, 'CrunchySeafood', 2, 1);
 INSERT INTO public.bateau VALUES (4, 'Chasse-Galerie', 10, 2);
 INSERT INTO public.bateau VALUES (5, 'Pirate bleu', 7, 3);
 INSERT INTO public.bateau VALUES (6, 'Le gentil ', 1, 3);
+INSERT INTO public.bateau VALUES (8, 'faux', 25, 1);
+INSERT INTO public.bateau VALUES (9, 'faux', 25, 1);
+INSERT INTO public.bateau VALUES (10, 'test', 5, 0);
+INSERT INTO public.bateau VALUES (11, 'voile', 1, 2);
+INSERT INTO public.bateau VALUES (12, 'voile arc-en-ciel', 5, 2);
 
 
 --
--- TOC entry 3327 (class 0 OID 24792)
--- Dependencies: 215
 -- Data for Name: flotte; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -105,17 +101,13 @@ INSERT INTO public.flotte VALUES (3, 'Drakars noirs');
 
 
 --
--- TOC entry 3337 (class 0 OID 0)
--- Dependencies: 217
 -- Name: bateau_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.bateau_id_seq', 6, true);
+SELECT pg_catalog.setval('public.bateau_id_seq', 12, true);
 
 
 --
--- TOC entry 3338 (class 0 OID 0)
--- Dependencies: 214
 -- Name: flotte_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -123,7 +115,6 @@ SELECT pg_catalog.setval('public.flotte_id_seq', 3, true);
 
 
 --
--- TOC entry 3183 (class 2606 OID 32999)
 -- Name: bateau bateau_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -132,15 +123,12 @@ ALTER TABLE ONLY public.bateau
 
 
 --
--- TOC entry 3181 (class 2606 OID 24799)
 -- Name: flotte flotte_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.flotte
     ADD CONSTRAINT flotte_pkey PRIMARY KEY (id);
 
-
--- Completed on 2023-09-27 14:41:07
 
 --
 -- PostgreSQL database dump complete
